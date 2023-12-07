06f89897dce431a66c75885c00577b0ae22244bd
Vinsx <vincentwhatwat175@gmail.com>
2023-12-07 15:32:39 +0700

    added main menu and start facade

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,6 +7,36 @@ import models.*;
 
 public class Main {
     public static void main(String[] args){
-        
+        int co = 0;
+        boolean call = true;
+        Scanner sc = new Scanner(System.in);
+        do{
+            do{
+                System.out.println("\n\n1. Main Menu"); 
+                System.out.println("2. Insert Data"); 
+                System.out.println("3. Show");
+                System.out.println("4. Exit");
+                try {
+                    co = sc.nextInt();
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+                sc.nextLine();
+            }while(co < 1 || co > 4);
+            switch (co) {
+                case 1:
+                    
+                    break;
+                case 2:
+                    // insertData();
+                    break;
+                case 3:
+                    // showData();
+                    break;
+                case 4:
+                    call = false;
+                    break;
+            }
+        }while(call);
     }
 }
--- src/repository/FacadeRep.java
+++ src/repository/FacadeRep.java
@@ -1,5 +1,10 @@
 package repository;
 
 public class FacadeRep {
-    
+    private TeamRepository teamRep;
+    private UserRepository userRep;
+    public FacadeRep(){
+        this.teamRep = new TeamRepository();
+        this.userRep = new UserRepository();
+    }
 }
